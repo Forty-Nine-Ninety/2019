7b6d8b943df40017d26f6a4dd82bcdf215406289
Benjamin A <yellowjaguar5@gmail.com>
2019-01-21 13:22:51 -0800

    Update SendableObject.java
    @majikalexplosions does this do the things?

--- src/main/java/frc4990/robot/SendableObject.java
+++ src/main/java/frc4990/robot/SendableObject.java
@@ -1,40 +1,56 @@
 package frc4990.robot;
 
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.SendableBase;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
-public class SendableObject extends SendableBase {
+public class SendableObject<number, bool, string> extends SendableBase {
 
-	public Supplier<?> data;
+	private Supplier<?> supplier;
 	/**
 	 * 
 	 * @param data The Supplier<?> you want to store
 	 * @author MajikalExplosions
 	 */
 	public SendableObject(Supplier<?> data) {
-		/*if (data.getClass().equals(new String().getClass()) ||
-		data.getClass().equals(new Double(0).getClass()) ||
-		data.getClass().equals(new String().getClass()) 
-		) */this.data = data;
-		//else throw new IllegalArgumentException("Value of type " + data.getClass().getName() + " cannot be put into a SendableObject");
+		this.supplier = data;
 	}
 
-	@SuppressWarnings("unchecked")
+	public double getDouble() {
+		return (double) supplier.get();
+	}
+
+	public boolean getBoolean() {
+		return (boolean) supplier.get();
+	}
+
+	public java.lang.String getString() {
+		return supplier.get().toString();
+	}
+
+	public double[] getDoubleArray() {
+		return (double[]) supplier.get();
+	}
+
+	public boolean[] getBooleanArray() {
+		return (boolean[]) supplier.get();
+	}
+
 	@Override
 	public void initSendable(SendableBuilder builder) {
-		if (data instanceof BooleanSupplier) {
-			builder.addBooleanProperty("value", (BooleanSupplier) data, null);
-		  } else if (data instanceof Number) {
-			builder.addDoubleProperty("value", (DoubleSupplier) data, null);
-		  } else if (data.getClass().equals((new String()).getClass())) {
-			builder.addStringProperty("value", (Supplier<String>) data, null);
+		if (supplier.get().getClass() == boolean.class) {
+				builder.addBooleanProperty("value", this::getBoolean, null);
+		  } else if (supplier.get().getClass() == double.class) {
+				builder.addDoubleProperty("value", this::getDouble, null);
+		  } else if (supplier.get().getClass() == "".getClass()) {
+				builder.addStringProperty("value", this::getString, null);
+			} else if (supplier.get().getClass() == boolean[].class) {
+				builder.addBooleanArrayProperty("value", this::getBooleanArray, null);
+			} else if (supplier.get().getClass() == double[].class) {
+				builder.addDoubleArrayProperty("value", this::getDoubleArray, null);
 		  } else {
-			throw new IllegalArgumentException("Value of type " + data.getClass().getName() + " cannot be put into a SendableObject");
+				throw new IllegalArgumentException("Value of type " + supplier.getClass().getName() + " cannot be put into a SendableObject");
 		  }
 	}
 }
