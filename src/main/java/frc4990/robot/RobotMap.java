382aab3b36dd56dc43fbb63a67c7363135475b16
Benjamin A <yellowjaguar5@gmail.com>
2019-02-03 09:13:19 -0800

    Update RobotMap.java

--- src/main/java/frc4990/robot/RobotMap.java
+++ src/main/java/frc4990/robot/RobotMap.java
@@ -18,6 +18,7 @@ import frc4990.robot.components.F310Gamepad;
 import frc4990.robot.components.TalonWithMagneticEncoder;
 import frc4990.robot.subsystems.Dashboard;
 import frc4990.robot.subsystems.DriveTrain;
+import frc4990.robot.subsystems.Pneumatic;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -27,11 +28,13 @@ import frc4990.robot.subsystems.DriveTrain;
  */
 
 public class RobotMap {
+	
+	public static PowerDistributionPanel pdp = new PowerDistributionPanel();
+	public static AHRS ahrs = new AHRS(SPI.Port.kMXP);
 
-	public static PowerDistributionPanel pdp;
+	public static F310Gamepad driveGamepad = new F310Gamepad(0);
+	public static F310Gamepad opGamepad = new F310Gamepad(1);
 
-	public static F310Gamepad driveGamepad;
-	public static F310Gamepad opGamepad;
 	public static TalonWithMagneticEncoder leftFrontDriveTalon;
 	public static WPI_TalonSRX leftRearDriveTalon;
 	public static TalonWithMagneticEncoder rightFrontDriveTalon;
@@ -40,70 +43,33 @@ public class RobotMap {
 	public static SpeedControllerGroup leftMotorGroup;
 	public static SpeedControllerGroup rightMotorGroup;
 
-	public static DriveTrain driveTrain;
+	public static Pneumatic pneumatic1 = new Pneumatic(0, 0);
+	public static Pneumatic pneumatic2 = new Pneumatic(0, 1);
+	public static DriveTrain driveTrain = new DriveTrain();
+	public static Dashboard dashboard = new Dashboard();
 
-	public static AHRS ahrs;
-
-	//public static Pneumatic pneumatic1;
-	//public static Pneumatic pneumatic2;
-
-	public static DigitalInput robotSelector;
-
-	public static Dashboard dashboard;
+	public static DigitalInput robotSelector = new DigitalInput(9); //true = practice bot, false = competition bot
 
 	public RobotMap() {
 
-		dashboard = new Dashboard();
-
-		robotSelector = new DigitalInput(9); //true = practice bot, false = competition bot
-
 		if (robotSelector.get()) { //practice bot
-
-			pdp = new PowerDistributionPanel();
-
-			driveGamepad = new F310Gamepad(0);
-			opGamepad = new F310Gamepad(1);
 
 			leftFrontDriveTalon = new TalonWithMagneticEncoder(22);
 			leftRearDriveTalon = new WPI_TalonSRX(9);
 			rightFrontDriveTalon = new TalonWithMagneticEncoder(6);
 			rightRearDriveTalon = new WPI_TalonSRX(21);
 
-			leftMotorGroup = new SpeedControllerGroup(leftFrontDriveTalon, leftRearDriveTalon);
-			rightMotorGroup = new SpeedControllerGroup(rightFrontDriveTalon, rightRearDriveTalon);
-
-			driveTrain = new DriveTrain();
-
-			ahrs = new AHRS(SPI.Port.kMXP);
-			//navX-MXP RoboRIO extension and 9-axis gyro thingy
-			//for simple gyro angles: use ahrs.getAngle() to get heading (returns number -n to n) and reset() to reset angle (and drift)
-
-			//pneumatic1 = new Pneumatic(0, 0);
-			//pneumatic2 = new Pneumatic(0, 1);
-
 		} else { //competition bot
-
-			pdp = new PowerDistributionPanel();
-
-			driveGamepad = new F310Gamepad(0);
-			opGamepad = new F310Gamepad(1);
 
 			leftFrontDriveTalon = new TalonWithMagneticEncoder(1);
 			leftRearDriveTalon = new WPI_TalonSRX(2);
 			rightFrontDriveTalon = new TalonWithMagneticEncoder(3);
 			rightRearDriveTalon = new WPI_TalonSRX(4);
 
-			leftMotorGroup = new SpeedControllerGroup(leftFrontDriveTalon, leftRearDriveTalon);
-			rightMotorGroup = new SpeedControllerGroup(rightFrontDriveTalon, rightRearDriveTalon);
-			
-			driveTrain = new DriveTrain();
-
-			ahrs = new AHRS(SPI.Port.kMXP);
-			//navX-MXP RoboRIO extension and 9-axis gyro thingy
-			//for simple gyro angles: use ahrs.getAngle() to get heading (returns number -n to n) and reset() to reset angle (and drift)
-
-			//pneumatic1 = new Pneumatic(0, 0);
-			//pneumatic2 = new Pneumatic(0, 1);
 		}
+
+		leftMotorGroup = new SpeedControllerGroup(leftFrontDriveTalon, leftRearDriveTalon);
+		rightMotorGroup = new SpeedControllerGroup(rightFrontDriveTalon, rightRearDriveTalon);
+
 	}
 }
