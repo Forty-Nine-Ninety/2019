b7580fc19d610fff7d92fe7f3fe03bd8bec2ea85
Benjamin A <yellowjaguar5@gmail.com>
2019-02-03 10:27:45 -0800

    condensed small commands into OI

--- src/main/java/frc4990/robot/OI.java
+++ src/main/java/frc4990/robot/OI.java
@@ -9,7 +9,10 @@ package frc4990.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc4990.robot.commands.*;
+import frc4990.robot.commands.TeleopDriveTrainController.StickShapingMode;
+import frc4990.robot.subsystems.Dashboard;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -41,8 +44,8 @@ public class OI{
 
 		
 		//controller check
-		RobotMap.driveGamepad.start.toggleWhenPressed(new ControllerCheck(RobotMap.driveGamepad));
-		RobotMap.opGamepad.start.toggleWhenPressed(new ControllerCheck(RobotMap.opGamepad));
+		RobotMap.driveGamepad.start.toggleWhenPressed(new InstantCommand("DriveControllerCheck", () -> System.out.println("START pressed on Drive Gamepad.")));
+		RobotMap.opGamepad.start.toggleWhenPressed(new InstantCommand("OPControllerCheck", () -> System.out.println("START pressed on OP Gamepad.")));
 
 		//Pneumatics
 		//RobotMap.opGamepad.x.whenPressed(new TogglePneumatic(RobotMap.pneumatic1));
@@ -198,5 +201,34 @@ public class OI{
 		}
 	}
 	
+	public class stickShapingToggle extends InstantCommand {
+
+		public stickShapingToggle() {}
+		
+		public void initialize() {
+			TeleopDriveTrainController.stickShapingMode = (TeleopDriveTrainController.stickShapingMode == StickShapingMode.DifferentialDrive) ? StickShapingMode.SquaredThrottle : StickShapingMode.DifferentialDrive;
+			System.out.println("[StickShaping Method] Changed to:" + TeleopDriveTrainController.stickShapingMode.toString());
 	
+		}
+		
+	}
+
+	public class DriveSpeedToggle extends InstantCommand {
+		public void initialize() {
+			TeleopDriveTrainController.currentThrottleMultiplier = TeleopDriveTrainController.currentThrottleMultiplier == 
+				Dashboard.getConst("DriveDpiToggle/lowThrottleMultiplier", 0.5) ? 
+				TeleopDriveTrainController.currentThrottleMultiplier = Dashboard.getConst("DriveDpiToggle/defaultThrottleMultiplier", 1.0) : 
+				Dashboard.getConst("DriveDpiToggle/lowThrottleMultiplier", 0.5);
+			System.out.println("Throttle Speed: " + TeleopDriveTrainController.currentThrottleMultiplier + "x");
+		}
+	}
+
+	public class TurnSpeedToggle extends InstantCommand {
+		public void initialize() {
+			TeleopDriveTrainController.currentTurnSteepnessMultiplier = TeleopDriveTrainController.currentTurnSteepnessMultiplier == 
+				Dashboard.getConst("TurnSpeedToggle/lowTurnMultiplier", 0.6) ? Dashboard.getConst("DriveDpiToggle/defaultTurnSpeedMultiplier", 1.0) : 
+				Dashboard.getConst("TurnSpeedToggle/lowTurnMultiplier", 0.6);
+			System.out.println("Turn Speed: " + TeleopDriveTrainController.currentTurnSteepnessMultiplier + "x");
+		}
+	}
 }
