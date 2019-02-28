79227b1f2499191c124bd58d04bf8df0b03ab2d4
gryphonrobotics (DS Computer 1) <gryphonrobotics@csus.org>
2019-02-27 16:52:36 -0800

    Update PIDTurretTurn.java

--- src/main/java/frc4990/robot/commands/PIDTurretTurn.java
+++ src/main/java/frc4990/robot/commands/PIDTurretTurn.java
@@ -15,15 +15,17 @@ public class PIDTurretTurn extends Command {
 	};
 
 	private double target;//in degrees
+	private TurretPoint point;
 
 
 	public PIDTurretTurn(double speed, TurretPoint point) {
 		super("PIDTurretTurn", RobotMap.turret);
 		target = getTarget(point);
+		this.point = point;
 	}
 
 	public void initialize() {
-		System.out.println("Initalizing PIDTurretTurn with target " + target+ " , at: " + RobotMap.turretTalon.getPosition());
+		System.out.println("Initalizing PIDTurretTurn with target " + point.toString() + " (" + target + ") , at: " + RobotMap.turretTalon.getPosition());
 		RobotMap.turretTalon.set(ControlMode.MotionMagic, target);
 	}
 
