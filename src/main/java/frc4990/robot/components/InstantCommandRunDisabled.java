c00d756270c27047a559dc18a7dae84f3b1831e2
Benjamin A <yellowjaguar5@gmail.com>
2019-03-14 10:37:09 -0700

    Update InstantCommandRunDisabled.java

--- src/main/java/frc4990/robot/components/InstantCommandRunDisabled.java
+++ src/main/java/frc4990/robot/components/InstantCommandRunDisabled.java
@@ -8,50 +8,18 @@
 package frc4990.robot.components;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * Add your docs here.
  */
 public class InstantCommandRunDisabled extends InstantCommand {
-		  
-    public InstantCommandRunDisabled() {
-        this.setRunWhenDisabled(true);
-    }
-
-    public InstantCommandRunDisabled(String name) {
-      super(name);
-      this.setRunWhenDisabled(true);
-    }
-
-    public InstantCommandRunDisabled(Subsystem subsystem) {
-      super(subsystem);
-      this.setRunWhenDisabled(true);
-    }
   
-    public InstantCommandRunDisabled(String name, Subsystem subsystem) {
-      super(name, subsystem);
-      this.setRunWhenDisabled(true);
-    }
-  
-    public InstantCommandRunDisabled(Runnable func) {
-      super(func);
-      this.setRunWhenDisabled(true);
-      
-    }
+  {
+    setRunWhenDisabled(true);
+  }
 
-    public InstantCommandRunDisabled(String name, Runnable func) {
-      super(name, func);
-      this.setRunWhenDisabled(true);
-    }
-  
-    public InstantCommandRunDisabled(Subsystem requirement, Runnable func) {
-      super(requirement, func);
-      this.setRunWhenDisabled(true);
-    }
-
-    public InstantCommandRunDisabled(String name, Subsystem requirement, Runnable func) {
-      super(name, requirement, func);
-      this.setRunWhenDisabled(true);
-    }
+  public InstantCommandRunDisabled(Runnable func) {
+    super(func);
+    
+  }
 }
